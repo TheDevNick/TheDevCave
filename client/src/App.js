90ab42b2d79ae84cab580bb7107c0c220419c4e5
Nick Hollins <nholl22@wgu.edu>
2023-01-03 14:09:06 -0600

    initial react setup

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import { Fragment } from "react";
+import {
+  BrowserRouter,
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
+const App = () => (
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+      </Routes>
+      <section className="container">
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </section>
+    </Fragment>
+  </Router>
+);
+
+export default App;
